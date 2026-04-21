ac1b97a4c373f46d4d1970cb62312ea6719d34f8
Kaushikee Singh Bhadauria <kaushikeesb05@gmail.com>
2026-04-21 14:24:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/More than n/k Occurrences/more-than-nk-occurrences.java
@@ -0,0 +1,18 @@
+class Solution {
+    // Function to find all elements in array that appear more than n/k times.
+    public int countOccurence(int[] arr, int k) {
+        // your code here,return the answer
+        int n =arr.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i =0; i<arr.length;i++){
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+        }
+        int count=0;
+        for(int i : map.values()){
+            if(i> n/k){
+                count++;
+            }
+        }
+        return count;
+    }
+}
